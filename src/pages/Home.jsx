ec3c1ee3c17c4ec7de9593e2a6a2c0fe4901a35b
hf16 <helmifarizki231@gmail.com>
2022-11-28 20:21:12 +0700

    UpdateTypePassword

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../assets/css/styleku.css";
 import Logo from "../assets/img/home-1.jpg";
-import Footer from "../component/Footer";
+// import Footer from "../component/Footer";
 // import Navigation from "../component/Nav";
 // import Nav from "../component/Nav"
 
@@ -10,7 +10,7 @@ function Home() {
     <>
       {/* <Navigation /> */}
       <img src={Logo} width="100%" alt="sda" />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
