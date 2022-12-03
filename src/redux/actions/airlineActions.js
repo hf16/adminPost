64035e010e6bd78350c6ca014feb18a2dedbf923
fransbachtiar24 <fransbachtiar5@mhs.mdp.ac.id>
2022-12-03 22:21:54 +0700

    lucu emng wkwk:v

--- src/redux/actions/airlineActions.js
+++ src/redux/actions/airlineActions.js
@@ -3,15 +3,20 @@ import { GET_AIRLINE_SUCCESS } from "../types";
 
 export const getListAirline = () => async (dispatch) => {
   try {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
 
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/airlines`, {
-      headers: { token },
-    });
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/airlines/all`,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
 
     dispatch({
       type: GET_AIRLINE_SUCCESS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (error) {
     throw error;
